5c1dfed397c4e276c4df79b06e50266868cd4733
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-27 19:52:03 +0530

    create common layout

---
+++ components/layout/main-header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./main-header.module.css"
+
+const MainHeader = props => {
+    return(
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link href="/">NextEvents</Link>
+            </div>
+            <nav className={styles.navigation}>
+                <ul>
+                    <li>
+                        <Link href="/events">All Events</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainHeader
