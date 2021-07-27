7dd548d7376763c4ca350789eb17ed1aa4f3759e
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-27 19:30:10 +0530

    edit event page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,26 @@
 import React from "react";
+import {useRouter} from "next/router";
+import {getEventById} from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 
 export default function EventDetailPage(){
+
+    const router = useRouter();
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if(!event){
+        return <p>event not found</p>
+    }
+
     return(
-        <div>
-            <h1>Event details</h1>
-        </div>
+        <>
+            <EventSummary/>
+            <EventLogistics/>
+            <EventContent/>
+        </>
     )
 }
