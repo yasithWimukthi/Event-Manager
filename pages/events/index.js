1b76375fe9313daa9c35600d07813857cc9ee787
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-30 18:54:29 +0530

    Implement search bar

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import {getAllEvents} from "../../dummy-data";
 import EventList from "../../components/events/event-list.component";
 import EventsSearch from "../../components/events/events-search";
+import {useRouter} from "next/router";
 
 export default function AllEventsPage(){
 
     const events = getAllEvents();
+    const router = useRouter();
+
+    const findEventsHandler = (year,month) => {
+        const path = `/events/${year}/${month}`
+        router.push(path)
+    }
 
     return(
         <div>
-            <EventsSearch/>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList items={events}/>
         </div>
     )
