f012a38102b62e5703290e2f7fb3593827c665dd
yasithWimukthi <it19966922@my.sliit.lk>
2021-08-01 17:37:20 +0530

    Implement search bar

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,39 @@
 import React from "react";
+import {useRouter} from "next/router";
+import {getFilteredEvents} from "../../dummy-data";
 
 export default function FilteredEventsPage(){
+
+    const router = useRouter();
+    const filterData = router.query.slug;
+
+    if(!filterData){
+        return <p className='center'>Loading...</p>
+    }
+
+    const filteredYear = +filterData[0];
+    const filteredMonth = +filterData[1];
+
+    if(
+        isNaN(filteredYear) ||
+        isNaN(filteredMonth) ||
+        filteredYear>2030 ||
+        filteredYear<2012 ||
+        filteredMonth < 1 ||
+        filteredMonth>12
+    ){
+        return <p>Invalid filter.</p>
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: filteredYear,
+        month: filteredMonth
+    });
+
+    if(!filteredEvents || filteredEvents.length === 0){
+        return <p>No events found!</p>
+    }
+
     return(
         <div>
             <h1>Filtered events</h1>
